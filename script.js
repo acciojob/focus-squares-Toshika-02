1aa3165d7b6985eda3a4e17197b4938a79ec8e34
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-31 12:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+const squares = document.querySelectorAll('.square');
+
+function changeColor(event) {
+    squares.forEach(square => {
+        if (square !== event.target) {
+            square.style.backgroundColor = '#6F4E37';
+        }
+    });
+}
+
+squares.forEach(square => {
+    square.addEventListener('mouseover', changeColor);
+        square.addEventListener('mouseout', () => {
+        squares.forEach(square => {
+            square.style.backgroundColor = '#E6E6FA';
+        });
+    });
+});
+
